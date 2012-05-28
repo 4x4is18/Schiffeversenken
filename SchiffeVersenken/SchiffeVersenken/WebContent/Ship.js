4d6d462d17f0815b7df66a537c0b87b643d1227c
michael.brand@uni-oldenburg.de <michael.brand@uni-oldenburg.de@4df026b4-6f93-4d64-86ed-6c7c0500be8c>
2012-05-28 15:50:35 +0000

    Angefangen zu kommentieren

--- SchiffeVersenken/SchiffeVersenken/WebContent/Ship.js
+++ SchiffeVersenken/SchiffeVersenken/WebContent/Ship.js
@@ -1,16 +1,57 @@
-Ship.COLOR = "rgba(0, 255, 255, 0.7)";
-Ship.COLOR_HIT = "rgba(255, 0, 0, 0.7)";
-Ship.NO_HIT = 0;
-Ship.HIT = 1;
+/*
+ * Ship - Klassenvariablen
+ */
+
+// Die Markierung fuer ein intaktes Schiffsteil:
+Ship.NO_HIT = 0;	// READ ONLY
+
+//Die Markierung fuer ein getroffenes Schiffsteil:
+Ship.HIT = 1;	// READ ONLY
+
+//Die Markierung fuer ein gesunkenes Schiff:
 Ship.SUNK_IN = 2;
 
+// Die Farbe, in der intakte Schiffsteile gezeichnet werden:
+Ship.COLOR = "rgba(0, 255, 255, 0.7)";	// READ ONLY
+
+// Die Farbe, in der der getroffene Schiffsteile gezeichnet werden:
+Ship.COLOR_HIT = "rgba(255, 0, 0, 0.7)";	// READ ONLY
+
+/*
+ * Ship - Instanzvariablen
+ */
+
+// TODO: von ID zu OWN
 Ship.prototype.id;
+// Die Angabe, ob das Schiff ein eigenes oder gegniersches ist:
+//Ship.prototype.own;
+
+// Die Laenge des Schiffes:
 Ship.prototype.length;
+
+// Die x-Koordinate des ersten Schiffsteils (links-oben):
 Ship.prototype.leftX;
+
+// Die y-Koordinate des ersten Schiffsteils (links oben):
 Ship.prototype.topY;
+
+// Die Angabe, ob das Schiff vertikal oder horizonal im Wasser liegt:
 Ship.prototype.vertical;
+
+// TODO: von HITS zu PARTS
+Ship.prototype.hits;
+// Das Array fuer die einzelnen Schiffsteile (je NO_HIT oder HIT)
+//Ship.prototype.parts;
+
+// TODO: NUMHITS rausnehmen
 Ship.prototype.numHits;
 
+/*
+ * Konstruktor zum Erstellen eines Schiffes.
+ */
+//TODO: von ID zu OWN
+//TODO: von HITS zu PARTS
+//TODO: NUMHITS rausnehmen
 function Ship(id, length, topY, leftX, vertical) {
 	
 	this.id = id;
@@ -28,12 +69,80 @@ function Ship(id, length, topY, leftX, vertical) {
 	
 }
 
+/*
+ * Getter fuer die Angabe, ob das Schiff ein eigenes oder gegniersches ist.
+ */
+Ship.prototype.isOwn = function() {
+	
+	return this.isOwn();
+	
+};
+
+/*
+ * Getter fuer die Laenge des Schiffes.
+ */
+//TODO: von ID zu OWN
+/*Ship.prototype.getLength = function() {
+	
+	return this.length;
+	
+};*/
+
+/*
+ * Getter fuer die x-Koordinate des ersten Schiffsteils (links-oben).
+ */
+Ship.prototype.getLeftX = function() {
+	
+	return this.leftX;
+	
+};
+
+/*
+ * Getter fuer die y-Koordinate des ersten Schiffsteils (links-oben).
+ */
+Ship.prototype.getTopY = function() {
+	
+	return this.topY;
+	
+};
+
+/*
+ * Getter fuer die Angabe, ob das Schiff vertikal oder horizontal im Wasser liegt.
+ */
+Ship.prototype.isVertical = function() {
+	
+	return this.vertical;
+	
+};
+
+/*
+ * Getter fuer die einzenen Schiffsteile.
+ * Fuer vertikale Schiffe: topY <= i < topY + length.
+ * Fuer horizontale Schiffe: leftX <= i < leftX + length.
+ */
+//TODO: von ID zu OWN
+/*Ship.prototype.getPart = function(i) {
+	
+	return this.parts[i];
+	
+}*/
+
+/*
+ * Zeichnen eines Schiffes.
+ */
+// TODO: von HITS zu PARTS
 Ship.prototype.draw = function(canvContext) {
 	
 	if(this.vertical) {
+		// Zeichnen eines vertikalen Schifes:
+		// Alle Schiffsteile von oben nach unten durchgehen und je nach dem, 
+		// ob es ein intaktes Schiffsteil oder ein getroffenes ist, die Farbe 
+		// waehlen.
 		
 		for(var y = this.topY; y < (this.topY + this.length); y++) {
 
+			// Erstes Teil: this.topY
+			// Letztes Teil this.topY + this.length
 			if(this.hits[y - this.topY]) {
 				
 				canvContext.fillStyle = Ship.COLOR_HIT;
@@ -43,15 +152,22 @@ Ship.prototype.draw = function(canvContext) {
 				canvContext.fillStyle = Ship.COLOR;
 				
 			}
+			
 			canvContext.fillRect(this.leftX * Board.FIELD_SIZE, y * Board.FIELD_SIZE, 
 					Board.FIELD_SIZE - 1, Board.FIELD_SIZE - 1);
 	        	
 		}
 		
 	} else {
+		// Zeichnen eines horizontalen Schifes:
+		// Alle Schiffsteile von links nach rechts durchgehen und je nach dem, 
+		// ob es ein intaktes Schiffsteil oder ein getroffenes ist, die Farbe 
+		// waehlen.
 
 		for(var x = this.leftX; x < (this.leftX + this.length); x++) {
 			
+			// Erstes Teil: this.leftX
+			// Letztes Teil this.leftX + this.length
 			if(this.hits[y]) {
 				
 				canvContext.fillStyle = Ship.COLOR_HIT;
@@ -61,6 +177,7 @@ Ship.prototype.draw = function(canvContext) {
 				canvContext.fillStyle = Ship.COLOR;
 				
 			}
+			
 			canvContext.fillRect(x * Board.FIELD_SIZE, this.topY * Board.FIELD_SIZE, 
 					Board.FIELD_SIZE - 1, Board.FIELD_SIZE - 1);
 	        	
@@ -70,13 +187,25 @@ Ship.prototype.draw = function(canvContext) {
 	
 };
 
+/*
+ * Ueberpruefung, ob das Schiff auf einem bestimmten Feld liegt.
+ * return: das Schiff oder null
+ */
 Ship.prototype.isOnField = function(y, x) {
 
-	if(this.vertical && x == this.leftX && y >= this.topY && y <= this.topY + this.length) {
+	if(this.vertical && x == this.leftX && y >= this.topY && 
+			y <= this.topY + this.length) {
+		// Vertikales Schiff:
+		// Erstes Teil: this.topY
+		// Letztes Teil this.topY + this.length
 
 		return this;
 		
-	} else if(!this.vertical && y == this.topY && x >= this.leftX && x < this.leftX + this.length) {
+	} else if(!this.vertical && y == this.topY && x >= this.leftX && 
+			x < this.leftX + this.length) {
+		// Horizontales Schiff:
+		// Erstes Teil: this.leftX
+		// Letztes Teil this.leftX + this.length
 		
 		return this;
 		
@@ -88,6 +217,7 @@ Ship.prototype.isOnField = function(y, x) {
 	
 };
 
+// TODO: rausnehmen?
 Ship.prototype.getHitPos = function(y, x) {
 	
 	if(this.vertical) {
